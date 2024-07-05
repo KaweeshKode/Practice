19a793ec05f48525b58879192e32cfa5d4dc4bca
KaweeshKode <gajinduksp@gmail.com>
2024-07-05 19:59:44 +0530

    JS DOM manipulation

--- app.js
+++ app.js
@@ -1,4 +1,69 @@
-console.log('Hello World!');
+let customers = [
+    {
+        name:"Saman",
+        age:25,
+        addres:"Colombo"
+    },
+    {
+        name:"Kamal",
+        age:30,
+        addres:"Kandy"
+    },
+    {
+        name:"Nimal",
+        age:35,
+        addres:"Galle"
+    },
+    {
+        name:"Sunil",
+        age:40,
+        addres:"Matara"
+    },
+    {
+        name:"Kasun",
+        age:45,
+        addres:"Jaffna"
+    },
+    {
+        name:"Rajitha",
+        age:76,
+        addres:"Anuradhapura"
+    }
+];
 
-console.log(5 == '5');
-console.log(5 === '5');
+function btnSubmit() {
+    addCustomer();
+    loadTable();
+};
+
+function addCustomer() {
+    let name = document.getElementById("txtName").value;
+    let age = document.getElementById("txtAge").value;
+    let address = document.getElementById("txtAddress").value;
+
+    customers.push({
+        name: name,
+        age: age,
+        addres: address
+    });
+};
+
+function loadTable() {
+    let tblCustomers = document.getElementById("tblCustomers");
+    
+    let tableBody = ` <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Address</th>
+                    </tr>`;
+    
+    customers.forEach(data => {
+        tableBody += `<tr>
+                        <td>${data.name}</td>
+                        <td>${data.age}</td>
+                        <td>${data.addres}</td>
+                     </tr>`;
+    });
+
+    tblCustomers.innerHTML = tableBody;
+};
