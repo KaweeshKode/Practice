d675e02eab857966f46d61fc08421f6129a84d28
KaweeshKode <gajinduksp@gmail.com>
2024-07-07 13:41:47 +0530

    updated searchbar for country search site.

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-let cardCountries = document.getElementById("cardCountries");
-
-let browserBody = "";
-
-fetch("https://restcountries.com/v3.1/all").then((res)=>res.json()).then(data=>{
-    data.forEach(element => {
-        browserBody += `<div class="col-3">
-                            <div class="card" style="width: 18rem; background-color:black; colour:white;">
-                                <img src="${element.flags.png}" class="card-img-top" alt="flags">
-                                <div class="card-body">
-                                    <h5 class="card-title">${element.name.common}</h5>
-                                    <p class="card-text">${element.name.official}</p>
-                                    <a href="${element.maps.googleMaps}" target="blank" class="btn btn-primary">Goto the Map</a>
-                                </div>
-                            </div>
-                        </div>`
-    });
-
-    cardCountries.innerHTML = browserBody;
-});
-
 function searchCountry() {
     let userInput = document.getElementById("txtInput").value;
-
-    console.log(userInput);
 
     let flagImg = document.getElementById("flagImg");
     let countryName = document.getElementById("countryName");
@@ -41,4 +18,28 @@ function searchCountry() {
 
         });
     });
+
+    document.getElementById("details").style.display = "";
 };
+
+
+let cardCountries = document.getElementById("cardCountries");
+
+let browserBody = "";
+
+fetch("https://restcountries.com/v3.1/all").then((res)=>res.json()).then(data=>{
+    data.forEach(element => {
+        browserBody += `<div class="col">
+                            <div class="card" style="width: 18rem;">
+                                <img src="${element.flags.png}" class="card-img-top" alt="flags">
+                                <div class="card-body">
+                                    <h5 class="card-title">${element.name.common}</h5>
+                                    <p class="card-text">${element.name.official}</p>
+                                    <a href="${element.maps.googleMaps}" target="blank" class="btn btn-primary">Goto the Map</a>
+                                </div>
+                            </div>
+                        </div>`
+    });
+
+    cardCountries.innerHTML = browserBody;
+});
